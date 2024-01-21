05dd0b3b413002f700a092e7b29587a81c10d45f
erik-pinhasov <erik944@gmail.com>
2024-01-21 19:51:11 +0200

    server youtube change

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ curl -o /dev/null https://www.youtube.com/
 curl -o /dev/null https://www.twitter.com/
 curl -o /dev/null https://www.snapchat.com/
 curl -o /dev/null https://www.instagram.com/p/C0wlbk6o1DI/
-wget https://www.instagram.com/p/C0wlbk6o1DI/
 curl -o /dev/null https://www.linkedin.com/
 
 
@@ -13,8 +12,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
-cp instaloadercontext.py ~/project/src/.venv/lib/python3.11/site-packages/instaloader/main.py
-cp main.py ~/project/src/.venv/lib/python3.11/site-packages/instaloader/main.py
 
 
 python manage.py collectstatic --no-input
--- socialDownloaderApp/mediaDownloaders/instagramDownloader.py
+++ socialDownloaderApp/mediaDownloaders/instagramDownloader.py
@@ -1,11 +1,13 @@
 import instaloader
 from socialDownloaderApp.mediaDownloaders.util.mediaHandler import downloadTempFile
 from socialDownloaderApp.mediaDownloaders.util.mediaHandler import formatVideoName
+from socialDownloaderApp.mediaDownloaders.util.requestHeaders import USER_AGENT
 
 
 def downloadInstagramVideo(url):
     try:
         insLoader = instaloader.Instaloader()
+        insLoader.context.user_agent = USER_AGENT
         media = instaloader.Post.from_shortcode(insLoader.context, url.split("/")[-2])
         videoName = formatVideoName(media.caption) if media.caption else "InstagramVideo"
         return downloadTempFile(media.video_url, videoName)
