8eca53ad9a891548ff37a4970f29217ed03d45c4
erik-pinhasov <erik944@gmail.com>
2024-01-21 18:40:38 +0200

    server youtube change

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 set -o errexit
 
-cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
-
 curl -o /dev/null https://www.facebook.com/
 curl -o /dev/null https://www.youtube.com/
 curl -o /dev/null https://www.twitter.com/
@@ -12,6 +10,8 @@ curl -o /dev/null https://www.linkedin.com/
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
 
 python manage.py collectstatic --no-input
 python manage.py migrate
