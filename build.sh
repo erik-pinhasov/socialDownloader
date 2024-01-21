45b029f0e1f3efbe5b892325ee1f96dfa0ed702c
erik-pinhasov <erik944@gmail.com>
2024-01-22 01:01:15 +0200

    server youtube change

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ curl -o /dev/null https://www.facebook.com/
 curl -o /dev/null https://www.youtube.com/
 curl -o /dev/null https://www.twitter.com/
 curl -o /dev/null https://www.snapchat.com/
-curl -o /dev/null https://www.instagram.com/p/C0wlbk6o1DI/
 curl -o /dev/null https://www.linkedin.com/
 
 
@@ -12,6 +11,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
+cp instaloadercontext.py ~/project/src/.venv/lib/python3.11/site-packages/instaloader/instaloadercontext.py
 
 
 python manage.py collectstatic --no-input
--- socialDownloaderApp/mediaDownloaders/instagramDownloader.py
+++ socialDownloaderApp/mediaDownloaders/instagramDownloader.py
@@ -7,6 +7,7 @@ from socialDownloaderApp.mediaDownloaders.util.requestHeaders import USER_AGENT
 def downloadInstagramVideo(url):
     try:
         insLoader = instaloader.Instaloader()
+        insLoader = insLoader.anonymous_copy()
         insLoader.context.user_agent = USER_AGENT
         media = instaloader.Post.from_shortcode(insLoader.context, url.split("/")[-2])
         videoName = formatVideoName(media.caption) if media.caption else "InstagramVideo"
