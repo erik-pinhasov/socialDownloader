87a5d7422f6eb4daeb911a2994d092b7cfe4f0dc
erik-pinhasov <erik944@gmail.com>
2024-01-22 03:02:04 +0200

    server config

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
-cp innertube.py ~/project/src/.venv/lib/python3.11/site-packages/pytube/innertube.py
+cp instaloadercontext.py ~/project/src/.venv/lib/python3.11/site-packages/instaloader/instaloadercontext.py
 
 python manage.py collectstatic --no-input
 python manage.py migrate
