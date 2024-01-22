a6c97961ff02820049f22c69cfc97823d80f5097
erik-pinhasov <erik944@gmail.com>
2024-01-22 04:34:26 +0200

    server config

--- socialDownloaderApp/mediaDownloaders/instagramDownloader.py
+++ socialDownloaderApp/mediaDownloaders/instagramDownloader.py
@@ -7,8 +7,6 @@ from socialDownloaderApp.mediaDownloaders.util.requestHeaders import USER_AGENT
 def downloadInstagramVideo(url):
     try:
         insLoader = instaloader.Instaloader()
-        insLoader.context.get_anonymous_session()
-        insLoader.login('erikpinhasov', 'Erer1515')
         insLoader.context.user_agent = USER_AGENT
         media = instaloader.Post.from_shortcode(insLoader.context, url.split("/")[-2])
         videoName = formatVideoName(media.caption) if media.caption else "InstagramVideo"
@@ -17,4 +15,3 @@ def downloadInstagramVideo(url):
     except Exception as e:
         print(f"Error in instagram download: {e}")
         return None
-downloadInstagramVideo('https://www.instagram.com/p/C0wlbk6o1DI/')
