d4fb18a1366e056f688cf7c8f29788332615930f
erik-pinhasov <erik944@gmail.com>
2024-01-23 02:53:25 +0200

    server config

--- socialDownloaderApp/settings.py
+++ socialDownloaderApp/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-DATABASES = {}
 
 
 ALLOWED_HOSTS = ['socialdownloader-kwtk.onrender.com', '127.0.0.1']
@@ -50,14 +49,22 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'socialDownloaderApp.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
